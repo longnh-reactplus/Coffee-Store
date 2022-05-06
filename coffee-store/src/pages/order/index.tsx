a39c46ba59561c5e4da7e2d652d598774e30e040
longks2312k <longks2312k@gmail.com>
2022-05-06 10:47:41 +0700

    add React Router

--- coffee-store/src/pages/order/index.tsx
+++ coffee-store/src/pages/order/index.tsx
@@ -1,9 +1,17 @@
+import { Col, Row } from 'antd'
 import React from 'react'
+import Header from '../../components/header'
 
 function Order() {
   return (
-    <div>
-        
+    <div style={{paddingLeft:100,paddingRight:100,marginTop:60}}>
+        <Header/>
+        <Row>
+          <Col span={5} style={{backgroundColor:'red'}}>
+            
+          </Col>
+          <Col span={19}></Col>
+        </Row>
     </div>
   )
 }
