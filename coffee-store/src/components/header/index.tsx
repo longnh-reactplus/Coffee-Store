3b22667303391fd6ca960c71e9dc818c52b9e999
longks2312k <longks2312k@gmail.com>
2022-05-06 13:59:34 +0700

    Menu

--- coffee-store/src/components/header/index.tsx
+++ coffee-store/src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import { Col, Image, Row } from 'antd'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from './navbar'
 
 function Header() {
@@ -8,7 +9,7 @@ function Header() {
     <div style={{height:60, width:"100%",position:'fixed',zIndex:100,top:0,backgroundColor:"rgba(255, 255, 255, 0.8)"}}>
       <Row style={{paddingRight:100,paddingLeft:100}}>
         <Col span={6}>
-          <Image preview={false} style={{height:60}} src="http://product.hstatic.net/200000377829/product/logo_tch_5a89533f917942b4993dabd0b031e6c6_grande.png"></Image>
+          <Image preview={false} style={{height:60}} src="http://product.hstatic.net/200000377829/product/logo_tch_5a89533f917942b4993dabd0b031e6c6_grande.png"><Link to={'/'}></Link></Image>
         </Col>
         <Col span={18}>
           <Navbar/>
--- coffee-store/src/pages/order/index.tsx
+++ coffee-store/src/pages/order/index.tsx
@@ -1,16 +1,66 @@
-import { Col, Row } from 'antd'
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Col, Menu, MenuProps, Row } from 'antd'
 import React from 'react'
 import Header from '../../components/header'
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuProps['items'] = [
+  getItem('Tất Cả', 'all'),
+
+  getItem('Cà Phê', 'coffee', null, [
+    getItem('Cà Phê Việt Nam', 'coffeeVN'),
+    getItem('Cà Phê Máy', 'coffeeMay'),
+    getItem('Cold Brew', 'coldbrew'),
+  ]),
+
+  getItem('Trà', 'tea', <SettingOutlined />, [
+    getItem('Trà trái cây', 'fruittea'),
+    getItem('Trà sữa Macchiato', 'milktea'),
+  ]),
+  getItem('Đá Xay', 'ice'),
+  getItem('Món Ăn Kèm', 'food'),
+  getItem('Tại nhà', 'home'),
+];
+
 function Order() {
+  const onClick: MenuProps['onClick'] = e => {
+    console.log('click ', e);
+  };
+
   return (
-    <div style={{paddingLeft:100,paddingRight:100,marginTop:60}}>
+    <div>
         <Header/>
-        <Row>
-          <Col span={5} style={{backgroundColor:'red'}}>
-            
+        <Row style={{paddingLeft:100,paddingRight:100,marginTop:60}}>
+          <Col span={5} style={{}}>
+          <Menu
+            onClick={onClick}
+            style={{ width: "100%" }}
+            defaultSelectedKeys={['all']}
+            defaultOpenKeys={['all']}
+            mode="inline"
+            items={items}
+          />
           </Col>
-          <Col span={19}></Col>
+          <Col span={19}>
+            <Row></Row>
+          </Col>
         </Row>
     </div>
   )
